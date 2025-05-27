97a4323365e5e31aa63fb7cf87967b02e1b91ba9
collamaf <francesco.collamati@lnf.infn.it>
2025-05-27 11:48:10 +0200

    Update run command to new API

--- DashboardFitLuGood.py
+++ DashboardFitLuGood.py
@@ -357,4 +357,4 @@ def update_plots(selected_range, selected_fraction, meas_to_generate, meas_durat
 
 # Esecuzione dell'app
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run(debug=True)
